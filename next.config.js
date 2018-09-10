d82a0ee55bcc9489081bc0117d54e5af7e5b412d
Romello Goodman <romellogood@gmail.com>
2018-09-09 20:08:41 -0400

    Updating deployment stuff like dockerfile and npm scripts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const serverPort = parseInt(process.env.PORT, 10) || 3000;
-const apiUrl = process.env.HOST || 'http://localhost:8080';
+const apiUrl = process.env.API_HOST || 'http://localhost:8080';
 
 module.exports = {
   serverRuntimeConfig: {},
